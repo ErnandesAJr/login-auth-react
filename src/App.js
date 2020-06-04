051805d548a427bcfb2f550faa0656d0d0255452
Ernandes Azevedo Junior <ernandes_azevedo@stash.atlantico.com.br>
2020-06-04 13:09:54 -0300

    Inserting routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-function App() {
-  return (
-    <div className="App">
+import Routes from './routes';
 
-    </div>
-  );
-}
+const App = () => <Routes></Routes>;
 
 export default App;
