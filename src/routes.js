7f2642b49b21af6cfc24ec3d450b3bcc8c3b73a0
Ernandes Azevedo Junior <ernandes_azevedo@stash.atlantico.com.br>
2020-06-04 11:26:03 -0300

    Initializing the project with create-react-app

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={() => <h1>Eita veio aqui</h1>}></Route>
+    </Switch>
+  </BrowserRouter>
+)
