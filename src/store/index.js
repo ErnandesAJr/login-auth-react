f77e193fdce8e75b0d59feaf78c79319d775264e
Ernandes Azevedo Junior <ernandes_azevedo@stash.atlantico.com.br>
2020-06-06 15:03:15 -0300

    So far it has been done with Redux-Thunk. A brief comment on its functionality

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,17 @@
 import { routerMiddleware } from "connected-react-router";
 import { applyMiddleware, createStore, compose } from "redux";
+/**
+ * 
+ In most cases, on smaller projects, using Thunk is more than enough if you need to make very few asynchronous requests and your application is not complex.
+  PROS
+    Uses Promise and can use async / await
+    Simplicity and ease
+  AGAINST
+    Callback hell
+    Complexity to climb
+    Complexity for writing tests
+    Promises cannot be canceled, so we do not have 100% control over our asynchronous shares.
+ */
 import thunk from "redux-thunk";
 import createRootReducer from "./ducks/index";
 import history from "../routes/history";
